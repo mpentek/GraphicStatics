643407f40fd69e36a4f5ec274fe21d70656b038d
mpentek <mpentek@gmail.com>
2018-09-28 13:04:02 +0200

    Creating input and output folders and files

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ Created on Sat May 26 13:57:06 2018
 
 @author: Benedikt
 """
+
 from matplotlib import pyplot as plt
+
 from plot import plot_system, plot_results, safe_to_pdf
 from Class_System import System
 from functions import calculate_member_forces, cremona_plan
+
+from os.path import join as join_path
 
 # close all open figures
 plt.close('all')
 
 # filename for import
-json_name = 'sample_ritter_input.json'
+#json_name = 'input/sample_ritter_input.json'
+input_folder = 'input'
+input_file_name = 'sample_ritter_input.json'
+json_name = join_path(input_folder, input_file_name)
+
+output_folder = 'output'
+output_file_name_prefix = 'report_'
 
 #create system and some atrributes from json file
 system = System(json_name)
@@ -48,7 +58,7 @@ ax_sub_fr_poly.set_title('Subsystem Force Polyon')
 ax_sub_fun_poly.set_title('Subsystem Funicular Polygon')
 
 ax_sub_fr_poly,ax_sub_fun_poly,fig_member_forces = calculate_member_forces(ax_sub_fr_poly,ax_sub_fun_poly,fig_member_forces,system)
- 
+
 # create cremona_plan
 fig_cremona = plt.figure('cremona',figsize = (11.69,8.27))
 ax_cremona = fig_cremona.add_subplot(111)
@@ -75,7 +85,8 @@ ax_results.axis('equal')
 plt.show()
 
 # Safe figures to pdf
-safe_to_pdf([fig_system,fig_reactions,fig_member_forces,fig_subsystem_forces,fig_cremona,fig_results],'test_01')
+safe_to_pdf([fig_system,fig_reactions,fig_member_forces,fig_subsystem_forces,fig_cremona,fig_results],
+            join_path(output_folder, output_file_name_prefix + input_file_name[:-5]))
 
 # ToDo: create output text document
 
